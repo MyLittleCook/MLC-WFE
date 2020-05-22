3391b6c8b99fc3f98a3b6dc63610425fed96635b
RRelease <jayhadas@naver.com>
2020-05-22 19:33:54 +0900

    첫 react 프로젝트 시작

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.scss';
+import Axios from 'axios';
+import logo from './assets/logo/Logo_M.png'
+
+const element = (
+    <div>
+        
+    </div>
+);
+
+class App extends Component {
+    render() {
+        return element;
+    }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname + "/build")
+  },
+  devServer: {
+    contentBase: path.resolve("./build"),
+    index: "index.html",
+    port: 9000
+  },
+  mode: "none",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: "/node_modules",
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|svg|jp(e*)g|ico)$/,
+        loader: 'url-loader',
+        options: { 
+            limit: 8000,
+            name: 'images/[hash]-[name].[ext]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+      favicon: './public/favicon.ico',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    }),
+    new CleanWebpackPlugin()
+  ]
+};
