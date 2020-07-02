d12e7ddb6635783201ec771c6b18aa45c884469f
RRelease <jayhadas@naver.com>
2020-07-02 23:52:03 +0900

    shareRecipe Info ui개발

--- src/component/ShareRecipe/contents/Info.js
+++ src/component/ShareRecipe/contents/Info.js
@@ -2,19 +2,45 @@ import React, { Component } from 'react';
 
 import './Info.scss';
 
+class Image extends Component {
+    state = {
+        file : '',
+        previewURL : ''
+    }
+
+    getImgFile = (e) => {
+        e.preventDefault();
+        let reader = new FileReader();
+        let file = e.target.files[0];
+        reader.onloadend = () => {
+            this.setState({
+                file : file,
+                previewURL : reader.result
+            })
+        }
+        reader.readAsDataURL(file);
+    }
+    render() {
+        return (
+            <div className="share-recipe__contents__info__img">
+                <label for="shareRecipeImgUpload">사진 업로드</label>
+                <input type="file" id="shareRecipeImgUpload" accept="image/*" onChange={this.getImgFile}/>
+                {this.state.file !== '' ? <img className='share-recipe__contents__info__img__preview' src={this.state.previewURL} /> : null}
+            </div>
+        )
+    }
+}
+
 class Info extends Component {
     render() {
         return(
             <article className="share-recipe__contents__info">
-                <div className="share-recipe__contents__info__img">
-                    <label for="shareRecipeImgUpload">사진 업로드</label>
-                    <input type="file" id="shareRecipeImgUpload" accept="image/*"/>
-                </div>
+                <Image />
                 <div className="share-recipe__contents__info__etc">
-                    <label for="shareRecipeName">레시피 이름</label>
-                    <input id="shareRecipeName" type="text"/>
-                    <label for="shareRecipeCategory">카테고리</label>
-                    <select id="shareRecipeCategory">
+                    <label className="share-recipe__contents__info__etc__title" for="shareRecipeName">레시피 이름</label>
+                    <input className="share-recipe__contents__info__etc__box" id="shareRecipeName" type="text"/>
+                    <label className="share-recipe__contents__info__etc__title" for="shareRecipeCategory">카테고리</label>
+                    <select className="share-recipe__contents__info__etc__box" id="shareRecipeCategory">
                         <option value="밥">밥</option>
                         <option value="반찬">반찬</option>
                         <option value="국&amp;찌개">국 &amp; 찌개</option>
@@ -24,12 +50,20 @@ class Info extends Component {
                     </select>
                 </div>
                 <div className="share-recipe__contents__info__ingredient">
-                    <div>
-                        <p></p>
-                        <div></div>
+                    <div className="share-recipe__contents__info__ingredient__top">
+                        <p>재료</p>
+                        <button></button>
                     </div>
-                    <div>
-                        <div>
+                    <div className="share-recipe__contents__info__ingredient__list">
+                        <div className="share-recipe__contents__info__ingredient__list__box">
+                            <p>당근</p>
+                            <p>1kg</p>
+                        </div>
+                        <div className="share-recipe__contents__info__ingredient__list__box">
+                            <p>당근</p>
+                            <p>1kg</p>
+                        </div>
+                        <div className="share-recipe__contents__info__ingredient__list__box">
                             <p>당근</p>
                             <p>1kg</p>
                         </div>
