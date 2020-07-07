0b32156ddbae76bd6d6daa7388406baaaf31934d
RRelease <jayhadas@naver.com>
2020-07-07 23:03:37 +0900

    category menu ui 수정

--- src/container/Recipe/search/SearchContainer.js
+++ src/container/Recipe/search/SearchContainer.js
@@ -3,24 +3,30 @@ import React, { Component } from 'react';
 import './SearchContainer.scss';
 import Search from '../../../component/Recipe/Search/Search';
 
+import all from '../../../assets/icon/all.png';
 import rice from '../../../assets/icon/rice.png';
 import sidedish from '../../../assets/icon/sidedish.png';
 import stew from '../../../assets/icon/stew.png';
 import dish from '../../../assets/icon/dish.png';
-import fried from '../../../assets/icon/fried.png';
-import bread from '../../../assets/icon/bread.png';
 import dessert from '../../../assets/icon/dessert.png';
+import etc from '../../../assets/icon/etc.png';
+import all_C from '../../../assets/icon/all_C.png';
 import rice_C from '../../../assets/icon/rice_C.png';
 import sidedish_C from '../../../assets/icon/sidedish_C.png';
 import stew_C from '../../../assets/icon/stew_C.png';
 import dish_C from '../../../assets/icon/dish_C.png';
-import fried_C from '../../../assets/icon/fried_C.png';
-import bread_C from '../../../assets/icon/bread_C.png';
 import dessert_C from '../../../assets/icon/dessert_C.png';
+import etc_C from '../../../assets/icon/etc_C.png';
 
 class SearchContainer extends Component {
     state = {
         categoryList: [
+            {
+                name: 'all',
+                icon: all,
+                icon_C: all_C,
+                txt: 'ALL'
+            },
             {
                 name: 'rice',
                 icon: rice,
@@ -46,22 +52,16 @@ class SearchContainer extends Component {
                 txt: '일품'
             },
             {
-                name: 'fried',
-                icon: fried,
-                icon_C: fried_C,
-                txt: '튀김'
-            },
-            {
-                name: 'bread',
-                icon: bread,
-                icon_C: bread_C,
-                txt: '빵'
-            },
-            {
                 name: 'dessert',
                 icon: dessert,
                 icon_C: dessert_C,
                 txt: '후식'
+            },
+            {
+                name: 'etc',
+                icon: etc,
+                icon_C: etc_C,
+                txt: '기타'
             },
         ]
     }
