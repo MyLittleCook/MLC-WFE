2c67102f97e2d8cf77d2c351a5979f8b31de1c99
RRelease <jayhadas@naver.com>
2020-07-07 23:07:55 +0900

    redux 코드 추가

--- src/component/Recipe/search/Search.js
+++ src/component/Recipe/search/Search.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { setRecipeSearch } from '../../../actions/index.js';
 
 import './Search.scss';
 import './CategoryBox.scss';
@@ -42,19 +44,16 @@ class Search extends Component {
         document.getElementById('recipeSerchInput').blur();
     }
 
+    inputEnter = (e) => {
+        const { addRecipeList } = this.props;
+        if(e.key === 'Enter') {
+            addRecipeList(e.target.value);
+        }
+    }
+
     render() {
         const { inputClicked } = this.state;
         const { categoryList } = this.props;
-
-        const inputEnter = (e) => {
-            if(e.key === 'Enter') {
-                
-            }
-        }
-
-        const changeCategory = (e) => {
-            
-        }
 
         const category = categoryList.map((data, i) => <CategoryBox icon={data.icon} icon_C={data.icon_C} txt={data.txt} name={data.name} key={i}/>);
 
@@ -63,7 +62,7 @@ class Search extends Component {
                 <article className="recipe__search__bar">
                     <div className={`recipe__search__${inputClicked ? 'bar--clicked' : 'bar--unclicked'}`}  onClick={this.inputClick} onBlur={this.inputClickOut}>
                         <img src={searchIcon}/>
-                        <input id="recipeSerchInput" type="text" placeholder={inputClicked ? '검색하실 레시피를 적어주세요.' : ''} onKeyDown={inputEnter} autoComplete="off"/>
+                        <input id="recipeSerchInput" type="text" placeholder={inputClicked ? '검색하실 레시피를 적어주세요.' : ''} onKeyDown={this.inputEnter} autoComplete="off"/>
                     </div>
                 </article>
                 <article className="recipe__search__category">
@@ -76,4 +75,11 @@ class Search extends Component {
     }
 }
 
-export default Search;
+const mapDispatchToProps = dispatch => ({
+    setRecipeSearch: text => dispatch(setRecipeSearch(text))
+})
+
+export default connect(
+    undefined,
+    mapDispatchToProps
+)(Search);
