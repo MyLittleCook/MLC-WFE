040b3cecdd4074a76a6c1c40bd6ed2126f8636f3
RRelease <jayhadas@naver.com>
2020-07-08 23:50:25 +0900

    IngredientModal 생성

--- src/component/ShareRecipe/contents/Info.js
+++ src/component/ShareRecipe/contents/Info.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import './Info.scss';
+import IngredientModal from '../modal/IngredientModal';
 
 import deleteIcon from '../../../assets/icon/delete.png';
 
@@ -34,34 +35,52 @@ class Image extends Component {
         return (
             <div className="share-recipe__contents__info__img">
                 <input type="file" id="shareRecipeImgUpload" accept="image/*" onChange={this.getImgFile}/>
-                {this.state.file !== '' ? <><img className='share-recipe__contents__info__img__preview' src={this.state.previewURL} /><img className='share-recipe__contents__info__img__delete' src={deleteIcon} onClick={this.deleteImgFile} /></> : <label for="shareRecipeImgUpload">사진 업로드</label>}
+                {this.state.file !== '' ? <><img className='share-recipe__contents__info__img__preview' src={this.state.previewURL} /><img className='share-recipe__contents__info__img__delete' src={deleteIcon} onClick={this.deleteImgFile} /></> : <label htmlFor="shareRecipeImgUpload">사진 업로드</label>}
             </div>
         )
     }
 }
 
 class Info extends Component {
+    state = {
+        IngredientModalShow: false,
+        data_category: '기타'
+    }
+
+    categoryChange = (e) => {
+        this.setState({
+            data_category: e.target.value
+        })
+    }
+
+    openIngredientModal = (b) => {
+        this.setState({
+            IngredientModalShow: b
+        })
+    }
+
     render() {
         return(
+            <>
             <article className="share-recipe__contents__info">
                 <Image />
                 <div className="share-recipe__contents__info__etc">
-                    <label className="share-recipe__contents__info__etc__title" for="shareRecipeName">레시피 이름</label>
+                    <label className="share-recipe__contents__info__etc__title" htmlFor="shareRecipeName">레시피 이름</label>
                     <input className="share-recipe__contents__info__etc__box" id="shareRecipeName" type="text"/>
-                    <label className="share-recipe__contents__info__etc__title" for="shareRecipeCategory">카테고리</label>
-                    <select className="share-recipe__contents__info__etc__box" id="shareRecipeCategory">
+                    <label className="share-recipe__contents__info__etc__title" htmlFor="shareRecipeCategory">카테고리</label>
+                    <select className="share-recipe__contents__info__etc__box" id="shareRecipeCategory" value={this.state.data_category} onChange={this.categoryChange}>
                         <option value="밥">밥</option>
                         <option value="반찬">반찬</option>
                         <option value="국&amp;찌개">국 &amp; 찌개</option>
                         <option value="일품">일품</option>
                         <option value="후식">후식</option>
-                        <option value="기타" selected>기타</option>
+                        <option value="기타">기타</option>
                     </select>
                 </div>
                 <div className="share-recipe__contents__info__ingredient">
                     <div className="share-recipe__contents__info__ingredient__top">
                         <p>재료</p>
-                        <button></button>
+                        <button onClick={() => {this.openIngredientModal(true)}}></button>
                     </div>
                     <div className="share-recipe__contents__info__ingredient__list">
                         <div className="share-recipe__contents__info__ingredient__list__box">
@@ -79,6 +98,8 @@ class Info extends Component {
                     </div>
                 </div>
             </article>
+            {this.state.IngredientModalShow ? <IngredientModal openIngredientModal={this.openIngredientModal}/> : null}
+            </>
         )
     }
 }
---
+++ src/component/ShareRecipe/modal/IngredientModal.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+import './IngredientModal.scss'
+
+class IngredientModal extends Component {
+    render() {
+        const { openIngredientModal } = this.props;
+
+        const removeEvent = () => {
+            e.stopPropagation();
+        }
+
+        return (
+            <div className="ingredient-modal" onClick={() => {openIngredientModal(false)}}>
+                <div className="ingredient-modal__box" onClick={removeEvent}>
+                    <div className="ingredient-modal__box__top">
+                        <p>재료 추가하기</p>
+                        <a>완료</a>
+                    </div>
+                    <div className="ingredient-modal__box__name">
+                        <input type="text" placeholder="식재료 이름" />
+                    </div>
+                    <div className="ingredient-modal__box__description">
+                        <input type="text" placeholder="식재료 설명" />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default IngredientModal;
