0fc27d410c3822eb32fd47e811726bc4636d5929
RRelease <jayhadas@naver.com>
2020-07-14 23:53:42 +0900

    aws s3에 이미지 업로드 코드 추가

---
+++ src/aws-s3-albums.js
@@ -0,0 +1,75 @@
+var albumBucketName = "mlc-images";
+var bucketRegion = "ap-northeast-2";
+var IdentityPoolId = "ap-northeast-2:9b272053-a85d-41af-8203-0234d993f6ae";
+
+AWS.config.update({
+    region: bucketRegion,
+    credentials: new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: IdentityPoolId
+    })
+});
+
+var s3 = new AWS.S3({
+    apiVersion: "2006-03-01",
+    params: { Bucket: albumBucketName }
+});
+
+export const createAlbum = (albumName) => {
+    let date = new Date();
+    albumName = `${date.getFullYear()}-${date.getMonth()}-${date.getDay()}-${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}_${albumName.trim().replace(/ /g, "_")}`;
+    console.log('first: ',albumName)
+    if (!albumName) {
+        return alert("Album names must contain at least one non-space character.");
+    }
+    if (albumName.indexOf("/") !== -1) {
+        return alert("Album names cannot contain slashes.");
+    }
+    var albumKey = encodeURIComponent(albumName);
+    s3.headObject({ Key: albumKey }, function(err, data) {
+        if (!err) {
+            return alert("Album already exists.");
+        }
+        if (err.code !== "NotFound") {
+            return alert("There was an error creating your album: " + err.message);
+        }
+        s3.putObject({ Key: albumKey }, function(err, data) {
+            if (err) {
+                return alert("There was an error creating your album: " + err.message);
+            }
+            console.log("Successfully created album.");
+            console.log('second: ', albumName)
+            return (albumName);
+        });
+    });
+}
+
+export const addPhoto = (albumName, file) => {
+    if (!file) {
+        return alert('Please choose a file to upload first.');
+    }
+    let fileName = file.name;
+    let albumPhotosKey = encodeURIComponent(albumName) + "/";
+  
+    let photoKey = albumPhotosKey + fileName;
+  
+    let upload = new AWS.S3.ManagedUpload({
+        params: {
+            Bucket: albumBucketName,
+            Key: photoKey,
+            Body: file,
+            ACL: "public-read"
+        }
+    });
+
+    let promise = upload.promise();
+
+    promise.then(
+        (data) => {
+            console.log("Successfully uploaded photo.");
+            console.log(data.Location)
+        },
+        (err) => {
+            return alert("There was an error uploading your photo: ", err.message);
+        }
+    );
+}
