c53c1a68e29f895a319cdae27a76dd6fb8c21744
RRelease <jayhadas@naver.com>
2020-07-24 22:51:39 +0900

    recipe listContainer 수정

--- src/container/Recipe/list/ListContainer.js
+++ src/container/Recipe/list/ListContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
-import { addRecipeList } from '../../../actions/index';
+import { toast } from 'react-toastify';
 
 import './ListContainer.scss';
 import ListWrapper from '../../../component/Recipe/list/ListWrapper';
@@ -10,7 +10,9 @@ import RecipeBox from '../../../component/Recipe/list/RecipeBox';
 class ListContainer extends Component {
     state = {
         recipePage: 1,
-        loadingData: false
+        loadingData: false,
+        searchFrom: '',
+        categoryFrom: ''
     }
 
     list = [];
@@ -20,8 +22,50 @@ class ListContainer extends Component {
         window.addEventListener("scroll", this.loadMoreData);
     }
 
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            recipePage: 1,
+            loadingData: true,
+            searchFrom: nextProps.recipeSearchString,
+            categoryFrom: nextProps.recipeCategoryString
+        })
+        this.list = [];
+        this.debounce(this.getData, 200)();
+    }
+
+    /*
+    static getDerivedStateFromProps(nextProps, prevState) {
+        if (nextProps.recipeSearchString !== prevState.searchFrom || nextProps.recipeCategoryString !== prevState.categoryFrom) {
+            this.list = [];
+            this.debounce(this.getData, 200)();
+            return { 
+                recipePage: 1,
+                loadingData: true,
+                searchFrom: nextProps.recipeSearchString,
+                categoryFrom: nextProps.recipeCategoryString
+            };
+        }
+        return null;
+    }
+    */
+
     componentWillUnmount() {
         window.removeEventListener("scroll", this.loadMoreData);
+    }
+
+    debounce(callback, milliseconds) {
+        let lastTimeoutId = null;
+        
+        return (...args) => {
+            if(lastTimeoutId) {
+                clearTimeout(lastTimeoutId);
+                lastTimeoutId = null;
+            }
+            lastTimeoutId = setTimeout(() => {
+                callback(...args);
+                lastTimeoutId = null;
+            }, milliseconds);
+        }
     }
 
     loadMoreData = () => {
@@ -37,38 +81,43 @@ class ListContainer extends Component {
     }
 
     getData = () => {
-        const { addRecipeList } = this.props;
+        const { recipePage, searchFrom, categoryFrom } = this.state;
 
-        // axios.get('https://picsum.photos/list')
-        // .then((response) => {
-        //     let result = response.data.slice(this.state.recipePage, this.state.recipePage+20);
-        //     result.forEach(d => {
-        //         this.list.push(d);
-        //     });
-        //     addRecipeList(this.list);
-        //     this.setState({
-        //         loadingData: false
-        //     })
-        // })
-        // .catch((request) => {
+        let params = {};
 
-        // })
+        if(searchFrom !== '') {
+            params = {
+                page: recipePage,
+                query: searchFrom
+            }
+        } else if(categoryFrom !== 'ALL' && categoryFrom !== '') {
+            params = {
+                page: recipePage,
+                category: categoryFrom
+            }
+        } else {
+            params = {
+                page: recipePage
+            }
+        }
+        console.log('params: ', params)
 
         axios.get('https://mlc.janghoseung.com/recipe/list', {
-            params: {
-                page: this.state.recipePage
-            }
+            params: params
         })
         .then((response) => {
-            console.log(response.data.result)
-            response.data.result.forEach(d => {
-                this.list.push(d);
-            });
-            addRecipeList(this.list);
-            this.setState({
-                recipePage: this.state.recipePage+1,
-                loadingData: false
-            })
+            if(response.data.result.length === 0) {
+                toast.error("레시피가 더이상 없습니다.");
+            } else {
+                console.log('response: ', response.data.result)
+                response.data.result.forEach(d => {
+                    this.list.push(d);
+                });
+                this.setState({
+                    recipePage: recipePage+1,
+                    loadingData: false
+                })
+            }
         })
         .catch((reject) => {
             console.log(reject)
@@ -76,11 +125,7 @@ class ListContainer extends Component {
     }
 
     render() {
-        const { recipeListArray } = this.props;
-
-        // let recipeBox = recipeListArray.map((data, i) => <RecipeBox titleImage={`https://picsum.photos/350/300/?image=${data.id}`} title={data.id} category={data.format} madeBy={data.author} type={0} key={i}/>)
-        let recipeBox = recipeListArray.map((data, i) => <RecipeBox titleImage={data.recipeImage} title={data.name} category={data.category} madeBy={data.author.nickname} type={data.category === "밥" ? 1 : (data.category === "반찬" ? 2 : (data.category === "국 & 찌개" ? 3 : (data.category === "일품" ? 4 : (data.category === "후식" ? 5 : 6))))} key={i}/>)
-
+        let recipeBox = this.list.map((data, i) => <RecipeBox titleImage={data.recipeImage} title={data.name} category={data.category} madeBy={data.author.nickname} type={data.category === "밥" ? 1 : (data.category === "반찬" ? 2 : (data.category === "국 & 찌개" ? 3 : (data.category === "일품" ? 4 : (data.category === "후식" ? 5 : 6))))} key={i}/>)
 
         return (
             <section className="recipe__list">
@@ -91,14 +136,10 @@ class ListContainer extends Component {
 }
 
 const mapStateToProps = (state) => ({
-    recipeListArray: state.recipe.recipeList
-})
-
-const mapDispatchToProps = dispatch => ({
-    addRecipeList: recipe => dispatch(addRecipeList(recipe))
-})
+    recipeSearchString: state.recipe.recipeSearch,
+    recipeCategoryString: state.recipe.recipeCategory
+});
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
+    mapStateToProps
 )(ListContainer);
