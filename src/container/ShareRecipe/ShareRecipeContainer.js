5a5018c549b33265fdd0edf09c0dbb0ff907f7cf
RRelease <jayhadas@naver.com>
2020-07-24 22:52:40 +0900

    shareRecipe data reset 코드 생성

--- src/container/ShareRecipe/ShareRecipeContainer.js
+++ src/container/ShareRecipe/ShareRecipeContainer.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { setSignInUpModalShow } from '../../actions/index';
+import { setSignInUpModalShow, setShareRecipeData, setRecipeImageFile, setStepImageFile } from '../../actions/index';
 import { toast } from 'react-toastify';
 
 import ContentsContainer from './contents/ContentsContainer';
 
 class ShareRecipe extends Component {
     componentDidMount() {
-        const { setSignInUpModalShow } = this.props;
+        const { setSignInUpModalShow, setShareRecipeData, setRecipeImageFile, setStepImageFile } = this.props;
+
         if(!sessionStorage.getItem('ACT')) {
             toast.error("로그인부터 해주세요");
             setSignInUpModalShow(true);
             this.props.history.push('/');
+        } else {
+            setShareRecipeData({
+                name: '',
+                ingredients: [],
+                category: '기타',
+                recipeImage: '',
+                steps: [
+                    {
+                        stepImage: '',
+                        content: '',
+                    }
+                ]
+            });
+            setRecipeImageFile(null);
+            setStepImageFile([]);
         }
     }
     render() {
@@ -24,10 +41,15 @@ class ShareRecipe extends Component {
 }
 
 const mapDispatchToProps = dispatch => ({
-    setSignInUpModalShow: bool => dispatch(setSignInUpModalShow(bool))
+    setSignInUpModalShow: bool => dispatch(setSignInUpModalShow(bool)),
+    setShareRecipeData: data => dispatch(setShareRecipeData(data)),
+    setRecipeImageFile: data => dispatch(setRecipeImageFile(data)),
+    setStepImageFile: data => dispatch(setStepImageFile(data))
 })
 
-export default connect(
-    undefined,
-    mapDispatchToProps
-)(ShareRecipe);
+export default withRouter(
+    connect(
+        undefined,
+        mapDispatchToProps
+    )(ShareRecipe)
+);
