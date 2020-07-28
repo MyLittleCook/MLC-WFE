40601b0546af5f8ba61915f0a614c27627411d3d
RRelease <jayhadas@naver.com>
2020-07-28 23:56:02 +0900

    로그인 안했을때 처리

--- src/container/Fridge/FridgeContainer.js
+++ src/container/Fridge/FridgeContainer.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setSignInUpModalShow } from '../../actions/index';
 import { toast } from 'react-toastify';
 
 import ListContainer from './list/ListContainer';
 import FridgeModal from '../../component/Fridge/modal/FridgeModal';
 
 class FridgeContainer extends Component {
+    componentDidMount() {
+        const { setSignInUpModalShow } = this.props;
+        
+        if(!sessionStorage.getItem('ACT')) {
+            toast.error("로그인부터 해주세요");
+            setSignInUpModalShow(true);
+            this.props.history.push('/');
+        }
+    }
 
     render() {
         return (
@@ -17,4 +28,13 @@ class FridgeContainer extends Component {
     }
 }
 
-export default FridgeContainer;
+const mapDispatchToProps = dispatch => ({
+    setSignInUpModalShow: bool => dispatch(setSignInUpModalShow(bool))
+})
+
+export default withRouter(
+    connect(
+        undefined,
+        mapDispatchToProps
+    )(FridgeContainer)
+);
