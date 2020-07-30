2b986d8281d3c25974aebe1440f1795509baaf00
RRelease <jayhadas@naver.com>
2020-07-30 23:47:52 +0900

    Add DetailRecipe Info

--- src/component/Recipe/detail/Info.js
+++ src/component/Recipe/detail/Info.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 import './Info.scss';
 
@@ -7,21 +8,59 @@ import heart from '../../../assets/icon/heart.png';
 import heart_C from '../../../assets/icon/heart_C.png';
 
 const Info = ({ recipeImage, title, category, ingredients, nickname, likes, isLiked, id }) => {
+    let liked = isLiked, like = likes;
+
+    const recipeLikeClicked = () => {
+        if(!liked) {
+            axios.post(`https://mlc.janghoseung.com/recipe/${id}/like`, {}, {
+                headers: {
+                    Authorization: sessionStorage.getItem('ACT')
+                }
+            })
+            .then(response => {
+                toast.success("레시피 좋아요 성공!");
+                liked = true;
+                like++;
+            })
+            .catch(reject => {
+                toast.error("레시피 좋아요 실패!");
+                console.log(reject);
+            })
+        } else {
+            axios.delete(`https://mlc.janghoseung.com/recipe/${id}/like`, {
+                headers: {
+                    Authorization: sessionStorage.getItem('ACT')
+                }
+            })
+            .then(response => {
+                toast.success('레시피 좋아요 삭제 성공');
+                liked = false;
+                like--;
+            })
+            .catch(reject => {
+                toast.error("레시피 좋아요 삭제 실패!");
+                console.log(reject)
+            })
+        }
+    }
+
     return (
         <article className="detail-recipe__info">
             <div className="detail-recipe__info__img">
                 <img src={recipeImage}/>
             </div>
             <div className="detail-recipe__info__txt">
-                <h1 className="detail-recipe__info__txt__title">{title}</h1>
-                <h3 className="detail-recipe__info__txt__category">{category}</h3>
-                <h2 className="detail-recipe__info__txt__ingredient">재료</h2>
-                <div className="detail-recipe__info__txt__ingredient-box">{ingredients}</div>
+                <div className="detail-recipe__info__txt__ticain">
+                    <h1 className="detail-recipe__info__txt__title">{title}</h1>
+                    <h3 className="detail-recipe__info__txt__category">{category}</h3>
+                    <h2 className="detail-recipe__info__txt__ingredient">재료</h2>
+                    <div className="detail-recipe__info__txt__ingredient-box">{ingredients}</div>
+                </div>
                 <div className="detail-recipe__info__txt__recike">
                     <p className="detail-recipe__info__txt__recipe-by">{`Recipe by ${nickname}`}</p>
                     <a className="detail-recipe__info__txt__like">
-                        <img src={isLiked ? heart_C : heart}/>
-                        <p>{likes}</p>
+                        <img src={isLiked ? heart_C : heart} onClick={recipeLikeClicked}/>
+                        <p>{like}</p>
                     </a>
                 </div>
             </div>
