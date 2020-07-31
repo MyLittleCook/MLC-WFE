7db4a9e158315b97872b35c28e712d5da2466b27
RRelease <jayhadas@naver.com>
2020-07-31 23:50:43 +0900

    minor alteration

--- src/component/Recipe/detail/Top.js
+++ src/component/Recipe/detail/Top.js
@@ -5,6 +5,10 @@ import { toast } from 'react-toastify';
 import './Top.scss';
 
 const Top = ({ isOwner, id, history }) => {
+    const recipeAlteration = () => {
+        toast.error('아직 준비되지 않은 기능입니다.');
+    }
+
     const recipeDelete = () => {
         axios.delete(`https://mlc.janghoseung.com/recipe/${id}`, {
             headers: {
@@ -26,7 +30,7 @@ const Top = ({ isOwner, id, history }) => {
         isOwner ?
         <>
             <article className="detail-recipe__top">
-                <a>수정</a>
+                <a className="detail-recipe__top__alteration" onClick={recipeAlteration}>수정</a>
                 <a className="detail-recipe__top__delete" onClick={recipeDelete}>삭제</a>
             </article>
         </>
--- src/component/Recipe/list/ListWrapper.js
+++ src/component/Recipe/list/ListWrapper.js
@@ -3,20 +3,33 @@ import React, { Component } from 'react';
 import './ListWrapper.scss'
 
 class ListWrapper extends Component {
+    state = {
+        viewList: false
+    }
+
+    changeView = () => {
+        const { viewList } = this.state;
+        if(viewList) {
+            this.setState({
+                viewList: false
+            })
+        } else {
+            this.setState({
+                viewList: true
+            })
+        }
+    }
+
     render() {
+        const { viewList } = this.state;
         const { recipes } = this.props;
         return (
             <div className="recipe__list__wrapper">
                 <article className="recipe__list__wrapper__top">
                     <a className="recipe__list__wrapper__top__newest">최신순</a>
                     <a className="recipe__list__wrapper__top__like">좋아요순</a>
-                    <div className="recipe__list__wrapper__top__card">
+                    <div className={`recipe__list__wrapper__top__${viewList ? 'list' : 'card'}`} onClick={this.changeView}>
                         <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                    </div>
-                    <div className="recipe__list__wrapper__top__list">
                         <div></div>
                         <div></div>
                         <div></div>
