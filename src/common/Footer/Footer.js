4ae51d2818face2fc0b6b1bca2a58b9b26f575e1
RRelease <jayhadas@naver.com>
2020-08-03 23:53:33 +0900

    Add footer

--- src/common/Footer/Footer.js
+++ src/common/Footer/Footer.js
@@ -7,7 +7,13 @@ class Footer extends Component {
     render() {
         return (
             <footer className="footer">
-
+                <div className="footer__contents">
+                    <a>ABOUT</a>
+                    <a>CONTACT</a>
+                    <a>INVESTORS</a>
+                    <a>DEVELOPER</a>
+                    <a>TERMS OF SERVICE</a>
+                </div>
             </footer>
         )
     }
